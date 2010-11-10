4a6a87531340a13af29f7ba08dbedfff847584cd
A.J. Cates <aj@ajcates.com>
2010-11-10 01:29:16 -0800

    added in multiList for convenince

--- index.js
+++ index.js
@@ -1,47 +1,66 @@
-var log = require("logging").from(__filename);
+var log = require("logging").from(__dirname);
 /*
 LSD - A module that lets you take "trips" thourgh callbacks.
 copyright @ajcates 2010
 MIT Licensed
 */
 
+exports.multiList = function(list, scope) {
+	list.map(function(i) {
+		return [scope.multi(), i];
+	}, this).forEach(function(i) {
+		i[0].apply(scope, i);
+	});
+};
+
 exports.dose = function() {
 	var rest = arguments,
-		dose = arguments.callee,
-		multiCount = 0,
-		allArgs = [];
-	
-	if( typeof( arguments[0] )  === "function" ) {
-		//set up a state that is useful for calling qued up funcs
-		var state = {
-			next : function() {
-				//calls the next callback.
-				return dose.apply(this, Array.prototype.slice.apply(rest, [1]));
-			},
-			multi : function() {
-				//factory function for handling the logic of multiple callbacks
-				multiCount++;
-				return function() {
-					allArgs.push(arguments);
-					multiCount--;
-					arguments = allArgs;
-					if(multiCount <= 0) {
-						log("arguments");
-						log(arguments);
-						return dose.apply(this, Array.prototype.slice.apply(rest, [1]));
-					}
-				};
-			}
-		};
-		//Call the first func and pass the parents caller args
-		return arguments[0].apply(state, arguments.callee.caller.arguments);
+		dose = arguments.callee;
+	if(rest.length) {
+		if( typeof( arguments[0] )  === "function" ) {
+			//set up a state that is useful for calling qued up funcs
+			var state = {
+				allArgs : [],
+				multiCount : 0,
+				multiList : exports.multiList,
+				next : function() {
+					//calls the next callback.
+					//arguments = allArgs;
+					//arguments.callee.caller.arguments
+					return dose.apply(this, Array.prototype.slice.apply(rest, [1]));
+				},
+				multi : function() {
+					//factory function for handling the logic of multiple callbacks
+					var $t = state;
+					$t.multiCount++;
+					
+					return function() {
+						log("multi func");
+						log($t);
+						
+						$t.allArgs.push(arguments);
+						$t.multiCount--;
+						if($t.multiCount <= 0) {
+							arguments = $t.allArgs;
+							//log("arguments");
+							//log(arguments);
+							
+							return dose.apply($t, Array.prototype.slice.apply(rest, [1]));
+						}
+					};
+				}
+			};
+			arguments[0].apply(state, Array.prototype.constructor.apply(this, arguments.callee.caller.arguments));
+		}
+		//return arguments.callee.apply(this, Array.prototype.slice.apply(rest, [1]));
 	}
-	return arguments[0];
-}
+	//return null;
+};
 
 exports.trip = function() {
 	//save a trip for later.
+	var args = arguments;
 	return function() {
-		exports.dose.apply(caller.arguments, this);
+		exports.dose.apply(this, args);
 	};
-}
+};
